a957b89fda91735062bb819b0f0ff2779cfedde4
Jeswin Kumar <jeswinpk@agilehead.com>
2018-01-27 17:59:27 +0530

    WIP: Nothing works yet

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import Server from "isotropy-webserver";
+
+const app = new Server();
+
+app.addRoutes([
+  [
+    "GET",
+    "/hello",
+    async ctx => {
+      ctx.body = "hello, world";
+    }
+  ]
+]);
+
